a86a683f2941be2be432082fb3fecffbc6917cac
Danilo <danilobatista@outlook.com.br>
2016-05-19 10:47:56 -0300

    teste

--- LivrariaES/src/br/com/livraria/controller/ImagesController.java
+++ LivrariaES/src/br/com/livraria/controller/ImagesController.java
@@ -16,20 +16,24 @@ import br.com.livraria.model.Livro;
 
 @ManagedBean
 @SessionScoped
-public class LivroImagesController {
+public class ImagesController {
 	
 	LivroDAO livroDAO = new LivroDAO();
 	
-    public StreamedContent getImage() throws IOException {
+    public StreamedContent mostrarImagem() throws IOException {
         FacesContext context = FacesContext.getCurrentInstance();
 
+        if(true){
+        	System.out.println("debug");
+        }
+        
         if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
             // So, we're rendering the HTML. Return a stub StreamedContent so that it will generate right URL.
             return new DefaultStreamedContent();
         }
         else {
             // So, browser is requesting the image. Return a real StreamedContent with the image bytes.
-            String studentId = context.getExternalContext().getRequestParameterMap().get("livroId");
+            String studentId = context.getExternalContext().getRequestParameterMap().get("livroID");
             Livro livro = livroDAO.buscarPorCodigo(Long.valueOf(studentId));
             return new DefaultStreamedContent(new ByteArrayInputStream(livro.getImagem()));
         }
